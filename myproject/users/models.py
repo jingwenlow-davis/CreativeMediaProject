f4430fc260ab13d351b06e31ed0589224d4cb5ea
Jingwen Low <jingwenlow@campus-029-122.ucdavis.edu>
2018-05-02 10:40:25 -0700

    django basic login

---
+++ myproject/users/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+# users/models.py
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    # First/last name is not a global-friendly pattern
+    name = models.CharField(blank=True, max_length=255)
+
+    def __str__(self):
+        return self.email
